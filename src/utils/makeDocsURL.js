3fc46072af62581b5a2ded74557014d9df1f4bd0
Kevin Deisz <kevin.deisz@gmail.com>
2020-04-27 13:54:31 -0400

    Move docs down one level

--- src/utils/makeDocsURL.js
+++ src/utils/makeDocsURL.js
@@ -1,4 +1,4 @@
 const makeDocsURL = (name) =>
-  `https://github.com/kddeisz/eslint-plugin-vue-accessibility/blob/master/docs/rules/${name}.md"`;
+  `https://github.com/kddeisz/eslint-plugin-vue-accessibility/blob/master/docs/${name}.md"`;
 
 module.exports = makeDocsURL;
