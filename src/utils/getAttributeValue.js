66aa6feb791b756d9b7fccd2b356a20d06a4cd23
Kevin Deisz <kevin.deisz@gmail.com>
2020-09-08 12:58:55 -0400

    Handle conditional expressions in :is

--- src/utils/getAttributeValue.js
+++ src/utils/getAttributeValue.js
@@ -10,7 +10,16 @@ const getAttributeValue = (node) => {
   }
 
   if (key.name.name === "bind" && value.expression) {
-    return value.type === "Literal" ? value.expression.value : value.expression;
+    // <div :height="100" />
+    if (value.expression.type === "Literal") {
+      return value.expression.value;
+    }
+
+    // TODO we're effectively using this as just a placeholder to let rules know
+    // that a value has been passed in for this attribute. We should replace
+    // this with a stronger API to either explicitly handel all of the different
+    // types of values or just return a special symbol or something else.
+    return value.expression;
   }
 
   return null;
--- src/utils/getElementType.js
+++ src/utils/getElementType.js
@@ -1,7 +1,17 @@
 const getElementAttributeValue = require("./getElementAttributeValue");
 const makeKebabCase = require("./makeKebabCase");
 
-const getElementType = (node) =>
-  makeKebabCase(getElementAttributeValue(node, "is") || node.rawName);
+const getElementType = (node) => {
+  let is = getElementAttributeValue(node, "is");
+
+  // If we could not parse the `is` value into a simple literal, we're going to
+  // have to ignore it because we're not smart enough to handle multiple values
+  // yet.
+  if (typeof is !== "string") {
+    is = null;
+  }
+
+  return makeKebabCase(is || node.rawName);
+};
 
 module.exports = getElementType;
