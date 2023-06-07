32071cb64d526f1acdb4177a69049e10eddcac3f
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2023-06-07 17:43:26 +0200

    docs: fix changelog url

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,7 +4,7 @@ import { version } from "../../package.json";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "vuejs-accessibility",
+  title: "eslint-plugin-vuejs-accessibility",
   description:
     "An eslint plugin for checking accessibility rules from within .vue files.",
   themeConfig: {
@@ -15,7 +15,7 @@ export default defineConfig({
         items: [
           {
             text: "Changelog",
-            link: "https://github.com/Lehoczky/vue-keycloak/blob/master/CHANGELOG.md"
+            link: "https://github.com/vue-a11y/eslint-plugin-vuejs-accessibility/blob/main/CHANGELOG.md"
           }
         ]
       }
