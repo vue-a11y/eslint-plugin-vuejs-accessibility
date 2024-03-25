86052617fddfe84c4ecb842658425e0de07edfae
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2024-03-25 13:47:51 +0100

    fix #932 not required switch prop

--- src/rules/__tests__/role-has-required-aria-props.test.ts
+++ src/rules/__tests__/role-has-required-aria-props.test.ts
@@ -3,11 +3,21 @@ import makeRuleTester from "./makeRuleTester";
 
 makeRuleTester("role-has-required-aria-props", rule, {
   valid: [
+    "<input role='switch' type='checkbox' aria-labelledby='test' />",
     "<span role='checkbox' aria-checked='false' aria-labelledby='test' tabindex='0' />",
     "<span :role='role' aria-checked='false' aria-labelledby='test' tabindex='0' />",
     "<span :role='role' />"
   ],
   invalid: [
+    {
+      code: "<input role='switch' aria-labelledby='test' />",
+      errors: [
+        {
+          messageId: "default",
+          data: { role: "switch", attributes: "aria-checked" }
+        }
+      ]
+    },
     {
       code: "<span role='checkbox' aria-labelledby='test' tabindex='0' />",
       errors: [
